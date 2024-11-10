f271a935d8fbcfab64b1c183795646d6b2937f56
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-11-10 10:05:48 +0200

    create DB tables in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
+import createTables from './scripts/tables';
 import app from './app';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+(async () => {
+    try {
+        await createTables(); 
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Error setting up the database:', error);
+        process.exit(1); // Exit if tables couldn't be created
+    }
+})();
