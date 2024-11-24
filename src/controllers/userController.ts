7b218ee20fa75c57ddfcf374a73b4617a5790c6a
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-11-24 20:53:53 +0200

    fix user controller and registerUser logic

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,19 +1,22 @@
-import { Request, Response } from 'express';
-import { v4 as uuidv4 } from 'uuid';
-import pool from '../config/database';
+import { NextFunction, Request, Response } from 'express';
+import { UserDto } from '../dtos/UserDTO';
+import { createNewUserService, checkIfEmailExists } from '../services/userServices';
+import errorHandler from '../utils/errorHandler'
+import APIError from '../utils/APIError';
+import bcrypt from 'bcryptjs';
 
-export const registerUser = async (req: Request, res: Response) => {
-    try {
-        const { name, email, password } = req.body;
-        const userId = uuidv4();  // Generate UUID
 
-        const [result] = await pool.query(
-            'INSERT INTO users (id, name, email, password) VALUES (?, ?, ?, ?)',
-            [userId, name, email, password]
-        );
-
-        res.status(201).json({ id: userId, name, email });
-    } catch (error) {
-        res.status(500).json({ message: 'Error registering user.' });
-    }
-};
+export const registerUser = errorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { fullName, email, password, invitationCode } = req.body;
+        const newUser = new UserDto(fullName, email, password, invitationCode)
+        if (await checkIfEmailExists(newUser.email)) {
+            return next(new APIError('Email already in use', 400));
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        newUser.password = hashedPassword;
+        createNewUserService(newUser);
+        res.status(201).json({
+            message: 'User registered successfully',
+        });
+    });
