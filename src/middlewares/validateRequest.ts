24c44fe9ca6254121eda43d53edd14c9d995029b
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-11-25 15:38:19 +0200

    enhance the request validation to handle multiple schemas formats

--- src/middlewares/validateRequest.ts
+++ src/middlewares/validateRequest.ts
@@ -1,13 +1,41 @@
 import { Request, Response, NextFunction } from 'express';
 import Joi from 'joi';
+import APIError from '../utils/APIError';
 
-export const validateRequest = (schema: Joi.Schema) => {
-    return (req: Request, res: Response, next: NextFunction): void => {
-        const { error } = schema.validate(req.body);
-        if (error) {
-            res.status(400).json({ message: error.details[0].message });
-        } else {
-            next();
-        }
-    };
+const validateRequest = (options: {
+  bodySchema?: Joi.ObjectSchema,
+  paramsSchema?: Joi.ObjectSchema,
+  querySchema?: Joi.ObjectSchema,
+}) => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    const { bodySchema, paramsSchema, querySchema } = options;
+    // Validate request body if bodySchema is provided
+    if (bodySchema && req.body) {
+      const { error } = bodySchema.validate(req.body, { abortEarly: false });
+      if (error) {
+        const errors: string = error.details.map((err) => err.message).join(', ');
+        return next(new APIError(errors, 400));
+      }
+    }
+
+    // Validate request params if paramsSchema is provided
+    if (paramsSchema && req.params) {
+      const { error } = paramsSchema.validate(req.params, { abortEarly: false });
+      if (error) {
+        const errors: string = error.details.map((err) => err.message).join(', ');
+        return next(new APIError(errors, 400));
+      }
+    }
+
+    // Validate request query if querySchema is provided
+    if (querySchema && req.query) {
+      const { error } = querySchema.validate(req.query, { abortEarly: false });
+      if (error) {
+        const errors: string = error.details.map((err) => err.message).join(', ');
+        return next(new APIError(errors, 400));
+      }
+    }
+    next();
+  };
 };
+export default validateRequest;
