33e625ac4865b47f8bde80b1c539b1f897c9c836
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-12-09 18:33:37 +0200

    validation for create user endpoint

--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -1,7 +1,50 @@
 import Joi, { ObjectSchema } from 'joi';
 
-export const userRegistrationSchema: ObjectSchema = Joi.object({
-    name: Joi.string().min(3).max(30).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required()
+
+export const registerNewUserSchema: ObjectSchema = Joi.object({
+    firstName: Joi.string()
+        .min(3)
+        .max(20)
+        .required()
+        .messages({
+            'string.base': 'First name must be a string',
+            'string.min': 'First name must be at least 3 characters long',
+            'string.max': 'First name must be less than or equal to 20 characters long',
+            'any.required': 'First name is required',
+        }),
+    lastName: Joi.string()
+        .min(3)
+        .max(20)
+        .required()
+        .messages({
+            'string.base': 'Last name must be a string',
+            'string.min': 'Last name must be at least 3 characters long',
+            'string.max': 'Last name must be less than or equal to 20 characters long',
+            'any.required': 'Last name is required',
+        }),
+    email: Joi.string().email({ tlds: { allow: false } }).required().messages({
+        'string.email': 'Invalid email format',
+        'any.required': 'Email is required',
+    }),
+    password: Joi.string()
+        .min(8)
+        .required()
+        .pattern(/[A-Z]/, 'uppercase letter')
+        .pattern(/[a-z]/, 'lowercase letter')
+        .pattern(/\d/, 'number')
+        .pattern(/[\W_]/, 'special character')
+        .messages({
+            'string.min': 'Password must be at least 8 characters long',
+            'string.pattern.name': 'Password must contain at least one {#name}',
+            'any.required': 'Password is required',
+        }),
+    invitationCode: Joi.string()
+        .length(36)
+        .required()
+        .messages({
+            'string.base': 'Invitation code must be a string',
+            'string.length': 'Invitation code must be exactly 36 characters long',
+            'any.required': 'Invitation code is required',
+        }),
+
 });
